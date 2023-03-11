86a45f15b65afac70032363178ec7db981b18864
Navod Peiris <navodpeiris1234@gmail.com>
2023-03-11 10:59:56 +0530

    first

---
+++ build/precache-manifest.982f7421700f46ab6bd451bb9ed83d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f6b6bd586661e6077fc",
+    "url": "/static/css/main.9f2c4989.chunk.css"
+  },
+  {
+    "revision": "9f6b6bd586661e6077fc",
+    "url": "/static/js/main.9f6b6bd5.chunk.js"
+  },
+  {
+    "revision": "9107e3133d289f677b7a",
+    "url": "/static/css/1.7975662b.chunk.css"
+  },
+  {
+    "revision": "9107e3133d289f677b7a",
+    "url": "/static/js/1.9107e313.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1eb92900345d37d78a26e6a782d0d5cf",
+    "url": "/index.html"
+  }
+];
